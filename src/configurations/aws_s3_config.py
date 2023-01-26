e400cb60b12a270d19f0fa0bdc36d444d89e360d
Shibendra Bhattacharjee <storebshiv@gmail.com>
2023-01-26 15:35:11 +0530

    AWS-Pipeline Added

---
+++ src/configurations/aws_s3_config.py
@@ -0,0 +1,12 @@
+import os
+from from_root import from_root
+from src.constants.cloud_constants import AWS_ACCESS_KEY_ID,AWS_REGION_NAME,AWS_SECRET_ACCESS_KEY,S3_TRAINING_BUCKET_NAME
+class AwsStorage:
+    def __init__(self):
+        self.ACCESS_KEY_ID = os.getenv(AWS_ACCESS_KEY_ID)
+        self.SECRET_KEY = os.getenv(AWS_SECRET_ACCESS_KEY)
+        self.REGION_NAME = os.getenv(AWS_REGION_NAME)
+        self.BUCKET_NAME = S3_TRAINING_BUCKET_NAME
+
+    def get_aws_storage_config(self):
+        return self.__dict__
